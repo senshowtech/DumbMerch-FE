2abea564b8910599a86e6a1de1693f406c205b41
sena <hargiantofebrisena@gmail.com>
2022-04-17 09:58:04 +0700

    initial

--- src/component/DetailComponent.js
+++ src/component/DetailComponent.js
@@ -1,11 +1,65 @@
 import "../assets/css/detail.css";
 import { Link, useLocation } from "react-router-dom";
 import { dataProduk } from "../dummy/dataProduk";
-import OngkirComponent from "./OngkirComponent";
+import { Form } from "react-bootstrap";
+import axios from "axios";
+import React from "react";
 
 const DetailComponent = () => {
   const { state } = useLocation();
   let data_detail = dataProduk[state.id];
+  const [province, setProvince] = React.useState([]);
+  const [namaKota, setnamaKota] = React.useState([]);
+  const [kotaTujuan, setkotaTujuan] = React.useState(0);
+  const [layanan, setLayanan] = React.useState([]);
+  const [hasilAkhir, sethasilAkhir] = React.useState(0);
+
+  React.useEffect(() => {
+    const getProvince = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/provinsi");
+        setProvince(response.data.rajaongkir.results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProvince();
+  }, []);
+
+  const HandleNamaKota = async (e) => {
+    const provinsi = e.target.value;
+    try {
+      const response = await axios.get(
+        `http://localhost:5000/kota/${provinsi}`
+      );
+      setnamaKota(response.data.rajaongkir.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const HandleKotaTujuan = (e) => {
+    const kota = e.target.value;
+    setkotaTujuan(kota);
+  };
+
+  const Layanan = async (e) => {
+    const kurir = e.target.value;
+    const kota_tujuan = kotaTujuan;
+    try {
+      const response = await axios.get(
+        `http://localhost:5000/ongkos/1/${kota_tujuan}/1/${kurir}`
+      );
+      setLayanan(response.data.rajaongkir.results[0].costs);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const Hasil = (e) => {
+    const hasil = e.target.value;
+    sethasilAkhir(hasil);
+  };
 
   let formatter = new Intl.NumberFormat("id-ID", {
     style: "currency",
@@ -27,10 +81,57 @@ const DetailComponent = () => {
           <h1 className="judul-produk">{data_detail.nama}</h1>
           <p className="paragrapgh-produk">{`Stock: ${data_detail.stok}`}</p>
           <p className="paragrapgh-produk">{data_detail.deskripsi}</p>
-          <OngkirComponent />
+          <Form.Select
+            onChange={HandleNamaKota}
+            name="provinsi"
+            className="select-pembayaran"
+          >
+            <option defaultValue={"Pilih"}>Pilih Provinsi</option>
+            {province.map((value) => {
+              return (
+                <option key={value.province_id} value={value.province_id}>
+                  {value.province}
+                </option>
+              );
+            })}
+          </Form.Select>
+          <Form.Select
+            onChange={HandleKotaTujuan}
+            className="mt-4 select-pembayaran"
+            name="kota"
+          >
+            <option defaultValue={"Pilih"}>Pilih Kota</option>
+            {namaKota.map((value) => {
+              return (
+                <option key={value.city_id} value={value.city_id}>
+                  {value.city_name}
+                </option>
+              );
+            })}
+          </Form.Select>
+          <Form.Select
+            className="mt-4 select-pembayaran"
+            name="kurir"
+            onChange={Layanan}
+          >
+            <option>Pilih Kurir</option>
+            <option value="jne">JNE</option>
+            <option value="pos">POS</option>
+            <option value="tiki">TIKI</option>
+          </Form.Select>
+          <Form.Select className="mt-4 select-pembayaran" onChange={Hasil}>
+            <option>Pilih Tipe Pengiriman</option>
+            {layanan.map((value, i) => {
+              return (
+                <option value={value.cost[0].value} key={i}>
+                  {value.service}
+                </option>
+              );
+            })}
+          </Form.Select>
           <div className="d-flex justify-content-end">
             <h3 className="judul-produk">
-              {formatter.format(data_detail.harga)}
+              {formatter.format(data_detail.harga + parseInt(hasilAkhir))}
             </h3>
           </div>
           <Link to="/profile" className="btn button-detail">
