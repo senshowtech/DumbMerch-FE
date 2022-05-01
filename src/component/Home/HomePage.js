c6cf00e7af0d7e2b16be187b5fa01f6bde53dfa7
sena <hargiantofebrisena@gmail.com>
2022-05-01 19:34:20 +0700

    initial

--- src/component/Home/HomePage.js
+++ src/component/Home/HomePage.js
@@ -1,14 +1,43 @@
 import "../../assets/css/home.css";
-import { dataProduk } from "../../dummy/dataProduk";
 import CardHome from "./CardHome";
 import React from "react";
 import { Form } from "react-bootstrap";
+import { API } from "../../config/axios";
 
 const HomePage = () => {
   const [values, setValues] = React.useState("");
+  const [products, setProducts] = React.useState({
+    product: null,
+    total_page: null,
+    current_page: null,
+    last_page: null,
+    total_data: null,
+  });
+  const [page, setPage] = React.useState(1);
 
-  let data_search = dataProduk.filter((value) => {
-    return value.nama.toLocaleLowerCase().includes(values.toLocaleLowerCase());
+  React.useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const response = await API.get("/products/" + page);
+        setProducts((prevState) => {
+          return {
+            ...prevState,
+            product: response.data.data.products,
+            total_page: response.data.data.total_page,
+            current_page: response.data.data.current_page,
+            last_page: response.data.data.last_page,
+            total_data: response.data.data.total_data,
+          };
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProduct();
+  }, [page]);
+
+  let data_search = products.product?.filter((value) => {
+    return value.title.toLocaleLowerCase().includes(values.toLocaleLowerCase());
   });
 
   return (
@@ -22,28 +51,28 @@ const HomePage = () => {
         className="form-search"
       />
       <div className="row">
-        {data_search.length === 0
-          ? dataProduk.map((value, index) => {
+        {data_search?.length === 1
+          ? products.product?.map((value) => {
               return (
                 <CardHome
                   key={value.id}
-                  id={index}
-                  nama={value.nama}
-                  harga={value.harga}
-                  stok={value.stok}
-                  gambar={value.gambar}
+                  id={value.id}
+                  nama={value.title}
+                  harga={value.price}
+                  stok={value.qty}
+                  gambar={value.image}
                 />
               );
             })
-          : data_search.map((value, index) => {
+          : data_search?.map((value) => {
               return (
                 <CardHome
                   key={value.id}
-                  id={index}
-                  nama={value.nama}
-                  harga={value.harga}
-                  stok={value.stok}
-                  gambar={value.gambar}
+                  id={value.id}
+                  nama={value.title}
+                  harga={value.price}
+                  stok={value.qty}
+                  gambar={value.image}
                 />
               );
             })}
